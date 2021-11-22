b62f782a27e7b2f126e037c385350477cd1fe334
mehedi <mehedibadhon294@gmail.com>
2021-11-22 08:57:36 +0600

    cout<<endl

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, add;
+    cout<<"Enter Two Numbers: ";
+    cin>>num1>>num2;
+    add = num1+num2;
+    cout<<"\nResult = "<<add;
+    cout<<endl;
+  cout<<endl;
+    return 0;
+}
